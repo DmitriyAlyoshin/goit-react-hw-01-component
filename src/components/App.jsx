4f23125d3d27342383ac7c71bbaa3aa4ff13cfb0
Dmitriy Alyoshin <valerich1715@gmail.com>
2023-05-28 19:38:31 +0200

    Correct styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,13 +11,14 @@ export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
+        
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
+        fontSize: 30,
         padding: 20,
-        color: '#010101',
+        color: '#020202',
       }}
     >
       <Profile
@@ -36,7 +37,6 @@ export const App = () => {
 
       {/* Transaction history from transactions.json */}
       <TransactionHistory transactions={transactions} />
-
     </div>
   );
 };
